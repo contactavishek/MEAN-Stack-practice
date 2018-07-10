56e5d7a454ec5c37d2e27dcc583f4f37050e9d25
contactavishek <contactavishek239@gmail.com>
2018-07-10 19:20:36 +0530

    Assignment 2 Task 2 complete

--- angular/confusion/src/app/about/about.component.ts
+++ angular/confusion/src/app/about/about.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Leader } from '../shared/leader';
+import { LEADERS } from '../shared/leaders';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  leaders: Leader[] = LEADERS;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
