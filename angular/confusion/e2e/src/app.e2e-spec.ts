cb7c3d9ee142c4dc67b4b29c361d469e8d720d4c
contactavishek <contactavishek239@gmail.com>
2018-08-15 19:43:28 +0530

    End-to-end test

--- angular/confusion/e2e/src/app.e2e-spec.ts
+++ angular/confusion/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,6 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
+import { protractor} from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +9,37 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
   });
+
+  it('should navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    const navlink = page.getAllElements('a').get(1);
+    navlink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    const EC = protractor.ExpectedConditions;
+    const newAuthor = page.getElement('input[type=text]');
+    browser.wait(EC.visibilityOf(newAuthor), 5000);
+    newAuthor.sendKeys('Test Author');
+
+    const newComment = page.getElement('textarea');
+    browser.wait(EC.visibilityOf(newComment), 5000);
+    newComment.sendKeys('Test Comment');
+
+    const newSubmitButton = page.getElement('button[type=submit]');
+    browser.wait(EC.visibilityOf(newSubmitButton), 5000);
+    newSubmitButton.click();
+
+    browser.pause(4200);
+  });
+
 });
